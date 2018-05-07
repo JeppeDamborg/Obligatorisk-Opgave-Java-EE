dc4e6a106ab6c144af3f33c8d7e3c910e08aed26
Tolga <Tolgasaj@Hotmail.com>
2018-05-07 11:19:30 +0200

    Har tilføjet flere Map metoder

--- Java EE.ejb/ejbModule/java_EE/ejb/Search.java
+++ Java EE.ejb/ejbModule/java_EE/ejb/Search.java
@@ -8,6 +8,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import domian.Exch;
+import domian.Mic;
+import java_EE.entity.Exchange_Code_Info;
 import java_EE.entity.Mapping_info;
 
 /**
@@ -27,6 +29,33 @@ public class Search implements SearchLocal {
 		exch.setIso_country(ex.getIso_country());
 		return exch;
 	}
+    
+    private Mapping_info map(Exch mi) {
+    	Mapping_info mapping_info = new Mapping_info();
+    	mapping_info.setExch_code(mi.getExch_code());
+    	mapping_info.setExch_name(mi.getExch_name());
+    	mapping_info.setComposite_code(mi.getComposite_code());
+    	mapping_info.setIso_country(mi.getIso_country());
+    	return mapping_info;
+    }
+    
+    private Mic map(Exchange_Code_Info mc) {
+    	Mic mic = new Mic();
+    	mic.setMic(mc.getMic());
+    	mic.setName(mc.getName());
+    	mic.setOperating_mic(mc.getOperating_mic());
+    	mic.setCorp_exchange(mc.getCorp_exchange());
+    	return mic;
+    }
+    
+    Exchange_Code_Info map(Mic eci) {
+    	Exchange_Code_Info exchange_Code_Info = new Exchange_Code_Info();
+    	exchange_Code_Info.setMic(eci.getMic());
+    	exchange_Code_Info.setName(eci.getName());
+    	exchange_Code_Info.setOperating_mic(eci.getOperating_mic());
+    	exchange_Code_Info.setCorp_exchange(eci.getCorp_exchange());
+    	return exchange_Code_Info;
+    }
 
 	@Override
 	public List<Exch> search(String exch) {
